1e4657e1f8a91fbc5d1c9ecdecf88c5fd69c1cca
AirenMarie <astringer92@gmail.com>
2024-07-01 23:48:28 -0400

    Create variables and first two functions

---
+++ roman-converter.js
@@ -0,0 +1,7 @@
+const numberInput = document.getElementById("number");
+const convertBtn = document.getElementById("convert-btn");
+const output = document.getElementById("output");
+
+const convertArabicToRoman = (input) => {};
+
+const checkInput = () => {};
